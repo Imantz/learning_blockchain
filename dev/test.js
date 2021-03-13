31298d349a1d5ab83de1a974212e134d7082e7ce
imants <imants.zuicans@gmail.com>
2021-03-13 14:58:09 +0200

    test

--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,11 @@ const Blockchain = require("./blockchain");
 const pokemonCoin = new Blockchain();
 
 pokemonCoin.createNewBlock(2390, "HASH", "ANOTHERHASH");
-pokemonCoin.createNewBlock(123, "ASD", "CCCUC");
-pokemonCoin.createNewBlock(32, "BUBUB", "JAS");
+pokemonCoin.createNewTransaction(100, "ALF", "BUBU");
+pokemonCoin.createNewBlock(123, "5435", "ASDE@!#");
+pokemonCoin.createNewTransaction(100, "ALF", "BUBU");
+pokemonCoin.createNewTransaction(300, "ALF", "BUBU");
+pokemonCoin.createNewTransaction(2000, "ALF", "BUBU");
+pokemonCoin.createNewBlock(132323, "ASDASD435", "GERKJGER");
 
 console.log(pokemonCoin);
