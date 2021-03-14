145980057007b9d363006e57fd0ddbab45d273e6
imants <imants.zuicans@gmail.com>
2021-03-14 16:24:02 +0200

    /register-note

--- dev/network-node.js
+++ dev/network-node.js
@@ -95,7 +95,15 @@ app.post("/register-and-broadcast-node", function (req, res) {
     });
 });
 
-app.post("/register-node", function (req, res) {});
+app.post("/register-node", function (req, res) {
+  const newNodeUrl = req.body.newNodeUrl;
+  const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) === -1;
+  const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+  if (nodeNotAlreadyPresent && notCurrentNode) {
+    bitcoin.networkNodes.push(newNodeUrl);
+    res.json({ note: "new node registered successfully with node" });
+  }
+});
 
 app.post("/register-nodes-bulk", function (req, res) {});
 app.listen(PORT, function () {
